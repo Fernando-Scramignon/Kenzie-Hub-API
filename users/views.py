96a43d19b82aaf5b0cccdd95a96b92df1e3bc905
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-03-09 15:34:23 -0300

    feat: creates the creation and deletion of users route

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework import generics
 
-class UserView(APIView):
-    def get(self, request):
-        return Response('I am working')
+from .serializers import UserSerializer
+from .models import User
+
+class UserView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
