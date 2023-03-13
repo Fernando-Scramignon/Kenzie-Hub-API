75d5d8ec49e63b7f9bebb5db60cc8e7cf07b1e38
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-03-13 17:29:31 -0300

    test: finishes user model tests

--- users/tests/test_model.py
+++ users/tests/test_model.py
@@ -34,9 +34,41 @@ class UserModelTests(TestCase):
             'contact': 256,
         }
 
+        cls.course_module_choices = [
+            'Primeiro módulo',
+            'Segundo módulo',
+            'Terceiro módulo',
+            'Quarto módulo',
+            'Não especificado'
+        ]
+
         cls.user_with_defaults = User.objects.create_user(**cls.user_data_with_defaults)
         cls.user_without_defaults = User.objects.create_user(**cls.user_data_without_defaults)
 
+
+    def test_course_modules_choices(self):
+        field_choices = User._meta.get_field('course_module').choices
+
+        for choice in self.course_module_choices:
+            choice in field_choices
+
+    def test_fields_defaults(self):
+        fields = self.defaults.keys()
+
+        for field in fields:
+            self.assertEqual(User._meta.get_field(field).default, self.defaults[field])
+
+    def test_email_unique_property(self):
+        self.assertTrue(User._meta.get_field('email').unique)
+
+    def test_avatar_url_null_property(self):
+        self.assertTrue(User._meta.get_field('avatar_url').null)
+
+    def test_max_length_rule(self):
+        fields = self.field_expected_length.keys()
+
+        for field in fields:
+            self.assertEqual(User._meta.get_field(field).max_length, self.field_expected_length[field])
 
     def test_creation_with_defaults(self):
         self.assertEqual(self.user_with_defaults.name, self.user_data_with_defaults['name'])
@@ -63,11 +95,3 @@ class UserModelTests(TestCase):
         self.assertTrue(self.user_without_defaults.id)
 
         self.assertIsNone(self.user_without_defaults.avatar_url)
-    
-    def test_max_length_rule(self):
-        fields = self.field_expected_length.keys()
-
-        for field in fields:
-            self.assertEqual(User._meta.get_field(field).max_length, self.field_expected_length[field])
-
-
