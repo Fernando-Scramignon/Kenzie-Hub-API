281292b816517a752a86855de531828910d02fd6
Fernando Scramignon <scramignonnarde@gmail.com>
2023-03-30 15:57:55 -0300

    feat: makes user not be able to register 2 techs with same name

--- works/views.py
+++ works/views.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # rest framework
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.exceptions import ValidationError
+from rest_framework.status import HTTP_409_CONFLICT
 
 # app
 from .models import Work
@@ -19,6 +21,18 @@ class WorkView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
+        works = self.request.user.works.all()
+        title = self.request.data['title']
+
+        is_title_unique = True
+
+        for work in works:
+            if work.title == title:
+                is_title_unique = False
+
+        if not is_title_unique:
+            raise ValidationError({'title': 'You alread registered this work'}, HTTP_409_CONFLICT)
+
         # saves user from Token (instead of directly from request data)
         serializer.save(user=self.request.user)
 
