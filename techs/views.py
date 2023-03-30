09f35893f322481e37c8b51638186577fbff286d
Fernando Scramignon <scramignonnarde@gmail.com>
2023-03-30 13:51:08 -0300

    feat: makes a user's tech to be unique to them

--- techs/views.py
+++ techs/views.py
@@ -1,5 +1,7 @@
 # rest framework
-from rest_framework.generics import ListCreateAPIView 
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.exceptions import ValidationError
+from rest_framework.status import HTTP_409_CONFLICT
 
 # authentication and authorization
 from rest_framework.permissions import IsAuthenticated
@@ -19,7 +21,22 @@ class TechView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
+    # It doesn't need to be unique, only unique amoung the user's techs
+
     def perform_create(self, serializer):
+
+        techs = self.request.user.techs.all()
+        title = self.request.data['title']
+
+        is_title_unique = True
+
+        for tech in techs:
+            if tech.title == title:
+                is_title_unique = False
+
+        if not is_title_unique:
+            raise ValidationError({'title':'you already registered this tech'}, HTTP_409_CONFLICT)
+
         # saves user from Token (instead of directly from request data)
         serializer.save(user=self.request.user)
     
