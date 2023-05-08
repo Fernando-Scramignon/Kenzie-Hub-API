ac081fc790ede8eacc5f10e868688742e1250bb6
Fernando Scramignon <scramignonnarde@gmail.com>
2023-05-07 21:25:58 -0300

    feat: creates tech detail view

--- techs/urls.py
+++ techs/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('techs/', views.TechView.as_view())
+    path('techs/', views.TechView.as_view()),
+    path('techs/<str:pk>/', views.TechDetailView.as_view())
 ]
--- techs/views.py
+++ techs/views.py
@@ -1,11 +1,12 @@
 # rest framework
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.exceptions import ValidationError
 from rest_framework.status import HTTP_409_CONFLICT
 
 # authentication and authorization
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from .permissions import IsOwner
 
 # app
 from .models import Tech
@@ -43,3 +44,29 @@ class TechView(ListCreateAPIView):
     def get_queryset(self):
         # only allow the current user tech's to be displayed
         return self.queryset.filter(user_id=self.request.user.id)
+
+class TechDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Tech.objects.all()
+    serializer_class = TechSerializer
+    
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    # It doesn't need to be unique, only unique amoung the user's techs
+
+    def perform_update(self, serializer):
+
+        techs = self.request.user.techs.all()
+        title = self.request.data['title']
+
+        is_title_unique = True
+
+        for tech in techs:
+            if tech.title == title:
+                is_title_unique = False
+
+        if not is_title_unique:
+            raise ValidationError({'title':'you already registered this tech'}, HTTP_409_CONFLICT)
+
+        # saves user from Token (instead of directly from request data)
+        serializer.save(user=self.request.user)
